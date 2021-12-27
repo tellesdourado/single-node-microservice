3ef7c9d6458a04f81c12cc8d0b599258fc050b84
Teles Dourado <teles.dourado.ext@dasa.com.br>
2021-12-27 13:16:07 -0300

    refactor: removing unessessary declarations

--- src/app/entities/event.ts
+++ src/app/entities/event.ts
@@ -1,6 +1,3 @@
-import { Controller } from "./controller";
-import { ActionFunction } from "./event-ingester";
-
 export interface EventRequest {
   message: string;
 }
@@ -9,10 +6,3 @@ export interface Route {
   to?: string;
   from: string;
 }
-
-// export interface Event {
-//   route(info: Route): this;
-//   post(message: string): Promise<void>;
-//   get(action: ActionFunction): Promise<void>;
-//   controller(ctrl: Controller): void;
-// }
